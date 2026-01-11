fe9d275831196bb3578e38b94b0a98a9f09f8a45
Frank_D_Chen <frank_d_chen@trendmicro.com>
2026-01-11 20:25:20 +0800

    fix: make shared event header BPF-friendly

--- include/ExecMonitor.h
+++ include/ExecMonitor.h
@@ -1,17 +1,14 @@
 #pragma once
 
-#include <stdint.h>
-
 #ifndef TASK_COMM_LEN
 #define TASK_COMM_LEN 16
 #endif
 
 // Event payload emitted by ebpf/exec_monitor.bpf.c via perf events.
 struct process_info_t {
-    uint32_t pid; // tgid
-    uint32_t uid;
-    uint32_t gid;
+    unsigned int pid; // tgid
+    unsigned int uid;
+    unsigned int gid;
     char comm[TASK_COMM_LEN];
     char filename[256];
 };
-
