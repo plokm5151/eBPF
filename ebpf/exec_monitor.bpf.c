6e8f2c2f786dd965d6cba93053584f364782cb87
Frank_D_Chen <frank_d_chen@trendmicro.com>
2026-01-12 12:40:37 +0800

    ebpf: avoid verifier issues in raw_syscalls handlers

--- ebpf/exec_monitor.bpf.c
+++ ebpf/exec_monitor.bpf.c
@@ -58,22 +58,6 @@ struct {
     __type(value, struct execve_args_t);
 } execve_args SEC(".maps");
 
-// Scratch space to avoid large stack frames (BPF stack is limited to 512 bytes).
-struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, struct execve_args_t);
-} scratch_args SEC(".maps");
-
-// Scratch space to avoid large stack frames (BPF stack is limited to 512 bytes).
-struct {
-    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-    __uint(max_entries, 1);
-    __type(key, __u32);
-    __type(value, struct process_info_t);
-} scratch_info SEC(".maps");
-
 SEC("tracepoint/raw_syscalls/sys_enter")
 int handle_sys_enter(struct trace_event_raw_sys_enter* ctx)
 {
@@ -89,16 +73,11 @@ int handle_sys_enter(struct trace_event_raw_sys_enter* ctx)
 
     __u64 pid_tgid = bpf_get_current_pid_tgid();
 
-    __u32 scratch_key = 0;
-    struct execve_args_t* args = bpf_map_lookup_elem(&scratch_args, &scratch_key);
-    if (!args) {
-        return 0;
-    }
-    __builtin_memset(args, 0, sizeof(*args));
+    struct execve_args_t args = {};
 
-    bpf_probe_read_user_str(args->filename, sizeof(args->filename), filename);
+    bpf_probe_read_user_str(args.filename, sizeof(args.filename), filename);
 
-    bpf_map_update_elem(&execve_args, &pid_tgid, args, BPF_ANY);
+    bpf_map_update_elem(&execve_args, &pid_tgid, &args, BPF_ANY);
     return 0;
 }
 
@@ -115,24 +94,18 @@ static __always_inline int handle_exit_exec_common(struct trace_event_raw_sys_ex
         return 0;
     }
 
-    __u32 scratch_key = 0;
-    struct process_info_t* info = bpf_map_lookup_elem(&scratch_info, &scratch_key);
-    if (!info) {
-        bpf_map_delete_elem(&execve_args, &pid_tgid);
-        return 0;
-    }
-    __builtin_memset(info, 0, sizeof(*info));
+    struct process_info_t info = {};
 
     __u64 uid_gid = bpf_get_current_uid_gid();
 
-    info->pid = pid_tgid >> 32;
-    info->uid = uid_gid & 0xffffffff;
-    info->gid = uid_gid >> 32;
-    bpf_get_current_comm(&info->comm, sizeof(info->comm));
+    info.pid = pid_tgid >> 32;
+    info.uid = uid_gid & 0xffffffff;
+    info.gid = uid_gid >> 32;
+    bpf_get_current_comm(&info.comm, sizeof(info.comm));
 
-    __builtin_memcpy(info->filename, args->filename, sizeof(info->filename));
+    __builtin_memcpy(info.filename, args->filename, sizeof(info.filename));
 
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, info, sizeof(*info));
+    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &info, sizeof(info));
 
     bpf_map_delete_elem(&execve_args, &pid_tgid);
     return 0;
